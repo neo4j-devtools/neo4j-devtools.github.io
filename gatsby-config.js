4be4ab7560a2f8696a599c8ca57dd353e5d21aaf
Andreas Kollegger <akollegger@gmail.com>
2020-08-21 22:55:56 +0200

    added neo4j logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `neo4j-developer-tools`,
+        short_name: `neo4j-devtools`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/neo4j-globe.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
